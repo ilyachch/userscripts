09ca0798b90e337ade34eb9f6861b806f4043931
Ilya Chichak <ilyachch@gmail.com>
2022-11-14 13:58:22 +0200

    in work

--- .userscript_template/{{ cookiecutter.slug }}/{{ cookiecutter.slug }}.user.js
+++ .userscript_template/{{ cookiecutter.slug }}/{{ cookiecutter.slug }}.user.js
@@ -4,21 +4,26 @@
 // @version      {{ cookiecutter.version }}
 // @description  {{ cookiecutter.description }}
 // @author       {{ cookiecutter.author }}
-// @homepageURL  {{ cookiecutter.repository }}
-// @supportURL   {{ cookiecutter.repository }}/issues
+// @homepageURL  {{ cookiecutter.__repository }}
+// @supportURL   {{ cookiecutter.__repository }}/issues
 // @updateURL    {{ cookiecutter.update_url }}
 // @downloadURL  {{ cookiecutter.update_url }}
 // @license      {{ cookiecutter.license }}
 
-// @noframes ???
-// @unwrap ???
-// @antifeature
+{%- if cookiecutter.noframes -%}
+// @noframes -
+{% endif %}
+
+{%- if cookiecutter.unwrap -%}
+// @unwrap
+{% endif %}
 // @run-at       document-start, document-end, document-idle
 // @grant        GM_addStyle
 // @connect      *
 // @require      *
 // @resource     *
 // @icon         *
+{{ cookiecutter.antifeatures|jsonify|indent(0) }}
 // ==/UserScript==
 
 (function() {
