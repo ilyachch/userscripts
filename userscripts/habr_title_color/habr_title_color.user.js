d018c85f9cb2ee5e9ff6f89ef3039eed62a27ee1
Ilya Chichak <ilyachch@gmail.com>
2022-11-22 15:46:02 +0200

    update harb_title_color

--- userscripts/habr_title_color/habr_title_color.user.js
+++ userscripts/habr_title_color/habr_title_color.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Habr title color
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  color article title based on article rating
 // @author       Ilya Chichak
 // @match        https://habr.com/*/post/*/*
@@ -14,31 +14,66 @@
 
 (function () {
     "use strict";
-    setTimeout(colorize_header, 500);
+    let rating = get_rating();
+    setTimeout(function () {
+        colorize_header(rating);
+        set_title(rating);
+    }, 500);
 })();
 
-function colorize_header() {
-    let title_block = document.querySelector("h1.tm-article-snippet__title");
-    let title_el = document.querySelector("h1.tm-article-snippet__title span");
+function Rating(
+    score,
+    side,
+    color = null,
+    sign = null,
+    should_colorize = false
+) {
+    this.score = score;
+    this.side = side;
+    this.color = color;
+    this.sign = sign;
+    this.should_colorize = should_colorize;
+}
+
+function get_rating() {
     let rating_el = document.querySelector(
         'span[data-test-id="votes-score-counter"]'
     );
-
-    let good_color = "#7aa600";
-    let bad_color = "#d04e4e";
-
-    if (title_block && rating_el) {
-        let accent_color = null;
-        if (rating_el.innerText.startsWith("+")) {
-            accent_color = good_color;
-        } else if (rating_el.innerText.startsWith("-")) {
-            accent_color = bad_color;
-        }
-        if (!!accent_color) {
-            title_block.style.color = accent_color;
-            if (!title_block.querySelector(".header_rating")) {
-                title_el.innerHTML += `<span style="color: ${accent_color}" class="header_rating"> (${rating_el.innerText})</span>`;
-            }
-        }
+    if (rating_el.innerText.startsWith("+")) {
+        return new Rating(
+            rating_el.innerText.slice(1),
+            "positive",
+            "#7aa600",
+            "+",
+            true
+        );
+    } else if (rating_el.innerText.startsWith("-")) {
+        return new Rating(
+            rating_el.innerText.slice(1),
+            "negative",
+            "#d04e4e",
+            "-",
+            true
+        );
+    } else {
+        return new Rating(0, "neutral");
     }
+}
+
+function colorize_header(rating) {
+    let title_block = document.querySelector("h1.tm-article-snippet__title");
+    let title_el = document.querySelector("h1.tm-article-snippet__title span");
+
+    if (!rating.should_colorize || !title_block) {
+        return;
+    }
+    if (!title_block.querySelector(".header_rating")) {
+        title_block.style.color = rating.color;
+        title_el.innerHTML += `<span style="color: ${rating.color}" class="header_rating"> (${rating.sign}${rating.score})</span>`;
+    }
+}
+
+function set_title(rating) {
+    let title_el = document.querySelector("head title");
+    title_el.innerText = `(${rating.sign}${rating.score}) ${title_el.innerText}`;
 }
