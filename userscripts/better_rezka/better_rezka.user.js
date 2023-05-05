1cf23a5f3a12d93803829150379c6b1acd251a0d
Ilya Chichak <ilyachch@gmail.com>
2023-05-05 17:41:36 +0300

    update better_rezka

--- userscripts/better_rezka/better_rezka.user.js
+++ userscripts/better_rezka/better_rezka.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better rezka script
 // @namespace    ilyachch/userscripts
-// @version      0.0.1
+// @version      0.1.0
 // @description  Custom Script - better_rezka
 // @author       ilyachch (https://github.com/ilyachch/userscripts)
 // @homepageURL  https://github.com/ilyachch/userscripts
@@ -18,7 +18,11 @@
 
 (function () {
     "use strict";
+    auto_next_episode();
+    add_year_links();
+})();
 
+function auto_next_episode() {
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             if (mutation.type === "childList") {
@@ -38,4 +42,31 @@
         childList: true,
         subtree: true,
     });
-})();
+}
+
+function add_year_links(){
+    if (!window.location.pathname.match(/\/\d{4}\//)) {
+        return
+    }
+    const year = parseInt(window.location.pathname.match(/\d{4}/)[0]);
+    const next_year = year + 1;
+    const prev_year = year - 1;
+
+    const header = document.querySelectorAll('.b-content__htitle h1')[0];
+
+    function make_link(url, text){
+        const link = document.createElement('a');
+        link.style.marginLeft = '10px';
+        link.href = url;
+        link.innerText = text;
+        return link;
+    }
+
+    const next_year_link = make_link(window.location.pathname.replace(/\d{4}\/.*/, `${next_year}/`), `${next_year}`);
+    const prev_year_link = make_link(window.location.pathname.replace(/\d{4}\/.*/, `${prev_year}/`), `${prev_year}`);
+
+    header.appendChild(prev_year_link);
+    if (year != new Date().getFullYear()) {
+        header.appendChild(next_year_link);
+    }
+}
