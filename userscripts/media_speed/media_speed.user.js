26a76f663c1fe75f16bb0ef01d652001e160502e
Ilya Chichak <ilyachch@gmail.com>
2023-08-03 11:51:31 +0300

    update media_speed to jump if not input is focused

--- userscripts/media_speed/media_speed.user.js
+++ userscripts/media_speed/media_speed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Media Speed
 // @namespace    ilyachch/userscripts/scripts
-// @version      0.1.3
+// @version      0.1.4
 // @description  Change media speed
 // @author       ilyachch (https://github.com/ilyachch/userscripts)
 // @homepageURL  https://github.com/ilyachch/userscripts
@@ -118,7 +118,7 @@ const SPEED_OPTIONS = [1, 1.5, 1.7, 2, 2.5, 3, 5, 10];
     );
 
     document.addEventListener("keydown", function (event) {
-        if (currentPlayingElement) {
+        if (currentPlayingElement && currentPlayingElement == document.activeElement) {
             if (event.shiftKey && event.code == "ArrowRight") {
                 currentPlayingElement.currentTime += 90;
             } else if (event.shiftKey && event.code == "ArrowLeft") {
