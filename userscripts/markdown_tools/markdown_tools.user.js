ea80cc8327804f3c304b71b740cd648de0c1a47e
Ilya Chichak <ilyachch@gmail.com>
2024-04-22 18:28:55 +0300

    fix format

--- userscripts/markdown_tools/markdown_tools.user.js
+++ userscripts/markdown_tools/markdown_tools.user.js
@@ -100,9 +100,7 @@ class MarkdownToolsMenu {
     static init() {
         document.addEventListener("keydown", (event) => {
             if (event.ctrlKey && event.shiftKey && event.key === "S") {
-                let menuElement = document.getElementById(
-                    MarkdownToolsMenu.elementId
-                );
+                let menuElement = document.getElementById(MarkdownToolsMenu.elementId);
 
                 if (menuElement === null) {
                     menuElement = MarkdownToolsMenu.create();
