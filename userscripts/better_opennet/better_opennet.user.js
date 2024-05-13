de102e2a6f899c2b5541276f6ee2965c151828fb
Ilya Chichak <ilyachch@gmail.com>
2024-05-13 12:12:55 +0300

    update opennet sript

--- userscripts/better_opennet/better_opennet.user.js
+++ userscripts/better_opennet/better_opennet.user.js
@@ -24,17 +24,19 @@
     remove_additional_controls();
 })();
 
-
-
 function remove_discussion() {
-    let discussion_header_element = document.querySelector('a[name="comments"]').closest("table.thdr");
+    let discussion_header_element = document
+        .querySelector('a[name="comments"]')
+        .closest("table.thdr");
     let discussion_element = discussion_header_element.nextElementSibling;
     discussion_header_element.style.display = "none";
     discussion_element.style.display = "none";
 }
 
 function remove_comment_form() {
-    let comment_form = document.querySelector('form[name="comment"]').closest('table.ttxt2');
+    let comment_form = document
+        .querySelector('form[name="comment"]')
+        .closest("table.ttxt2");
     let comment_form_header = comment_form.previousElementSibling;
     comment_form_header.style.display = "none";
     comment_form.style.display = "none";
